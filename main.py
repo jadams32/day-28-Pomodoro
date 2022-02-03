0720111ef451c632b8e9e8c1714f9bfdfe8a9dc4
John Adams Jr <john.c.adams32@gmail.com>
2022-02-02 22:54:03 -0700

    Base layout and buttons completed.

--- main.py
+++ main.py
@@ -27,8 +27,19 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_background = PhotoImage(file="tomato.png")
 canvas.create_image(100, 110, image=tomato_background)
-canvas.create_text(100, 135, text="00:00", fill= "white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
+reset_button = Button(text="RESET")
+reset_button.grid(column=2, row=2)
+
+start_button = Button(text="START")
+start_button.grid(column=0, row=2)
+
+level_check_mark = Label(text="✓", font=FONT_NAME, fg=GREEN, bg=YELLOW)
+level_check_mark.grid(column=1, row=3)
+
+timer_label = Label(text="TIMER", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
 
 window.mainloop()

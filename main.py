3e8153d2730cbfdbf3eacd8e70a7e681391cfacf
John Adams Jr <john.c.adams32@gmail.com>
2022-02-02 21:46:28 -0700

    base background updated

--- main.py
+++ main.py
@@ -22,11 +22,12 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_background = PhotoImage(file="tomato.png")
-canvas.create_image(103, 110, image=tomato_background)
+canvas.create_image(100, 110, image=tomato_background)
+canvas.create_text(100, 135, text="00:00", fill= "white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
 

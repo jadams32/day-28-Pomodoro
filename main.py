4f2402c2dc6f1e82462a74a02143a830632ba7b6
John Adams Jr <john.c.adams32@gmail.com>
2022-02-02 21:30:59 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# On day 28 I build a pomodoro app, using the GUI module tkinter. Use this app to up your productivity by
+# working in timed intervals.
+
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+
+window.mainloop()
